f2adadd9d3303e2880fb15bca89b64b0682cfbd2
keaimk <fly1043@naver.com>
2024-11-18 09:02:10 +0900

    최종 페이지 번호 계산을 위해 총 레코드 수 응답

--- backend/src/main/java/com/example/backend/controller/mapper/board/BoardMapper.java
+++ backend/src/main/java/com/example/backend/controller/mapper/board/BoardMapper.java
@@ -50,4 +50,9 @@ public interface BoardMapper {
             LIMIT #{offset}, 10
             """)
     List<Board> selectPage(Integer offset);
+
+    @Select("""
+            SELECT COUNT(*) FROM board
+            """)
+    Integer countAll();
 }
