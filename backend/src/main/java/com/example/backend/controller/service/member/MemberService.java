9a2af2e23ae9364ad8e7cda324e68709004677da
keaimk <fly1043@naver.com>
2024-11-19 14:59:26 +0900

    권한 테이블 생성
    trump에 admin, manager 권한 부여
    
    토큰 생성시 권한정보 (scope) 추가

--- backend/src/main/java/com/example/backend/controller/service/member/MemberService.java
+++ backend/src/main/java/com/example/backend/controller/service/member/MemberService.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -69,15 +70,18 @@ public class MemberService {
 
     public String token(Member member) {
         Member db = mapper.selectById(member.getId());
+        List<String> auths = mapper.selectAuthByMemberId(member.getId());
+        String authsString = auths.stream()
+                .collect(Collectors.joining(" "));
         if (db != null) {
             if (db.getPassword().equals(member.getPassword())) {
-//            token 만들어서 리턴
+                // token 만들어서 리턴
                 JwtClaimsSet claims = JwtClaimsSet.builder()
                         .issuer("self")
                         .subject(member.getId())
                         .issuedAt(Instant.now())
                         .expiresAt(Instant.now().plusSeconds(60 * 60 * 24 * 7))
-//                        .claim("scope", "")
+                        .claim("scope", authsString)
                         .build();
 
                 return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
---
+++ sql/auth.sql
@@ -0,0 +1,16 @@
+USE prj20241114;
+CREATE TABLE auth
+(
+    member_id VARCHAR(20) REFERENCES member (id),
+    auth      VARCHAR(20) NOT NULL,
+    PRIMARY KEY (member_id, auth)
+);
+#
+INSERT INTO auth (member_id, auth)
+VALUES ('trump', 'admin');
+INSERT INTO auth (member_id, auth)
+VALUES ('trump', 'manager');
+DESC member;
+
+SHOW CREATE TABLE auth;
+SHOW CREATE TABLE member;
