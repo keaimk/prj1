a01141463315fa67d8a82b81ac9c0e0bfa52c1b7
keaimk <fly1043@naver.com>
2024-11-20 11:29:05 +0900

    댓글 작성시 댓글 목록 업데이트를 위해 lifting state up함

--- frontend/src/components/comment/CommentInput.jsx
+++ frontend/src/components/comment/CommentInput.jsx
@@ -1,27 +1,18 @@
-import { Box, Group, Input, Textarea } from "@chakra-ui/react";
+import { Box, Group, Textarea } from "@chakra-ui/react";
 import { Button } from "../ui/button.jsx";
 import { useState } from "react";
-import axios from "axios";
 
-export function CommentInput({ boardId }) {
+export function CommentInput({ boardId, onSaveClick }) {
   const [comment, setComment] = useState("");
-
-  function handleSaveClick() {
-    axios
-      .post("/api/comment/add", { boardId: boardId, comment })
-      .then()
-      .catch()
-      .finally();
-  }
 
   return (
     <Box>
-      <Group attached>
+      <Group>
         <Textarea
-          vlaue={comment}
+          value={comment}
           onChange={(e) => setComment(e.target.value)}
         />
-        <Button onClick={handleSaveClick}>댓글 쓰기</Button>
+        <Button onClick={() => onSaveClick(comment)}>댓글 쓰기</Button>
       </Group>
     </Box>
   );
