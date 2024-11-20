66d61975bf341c8d552256e528884379b67183b9
keaimk <fly1043@naver.com>
2024-11-20 12:42:47 +0900

    댓글 삭제

--- backend/src/main/java/com/example/backend/dto/comment/Comment.java
+++ backend/src/main/java/com/example/backend/dto/comment/Comment.java
@@ -2,12 +2,10 @@ package com.example.backend.dto.comment;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 public class Comment {
+    private Integer id;
     private Integer boardId;
     private String memberId;
     private String comment;
-    private LocalDateTime inserted;
 }
