b2eea46ea779bda93bcddca9eaeb237299a1821a
keaimk <fly1043@naver.com>
2024-11-20 16:55:34 +0900

    파일 업로드

--- backend/src/main/java/com/example/backend/controller/board/BoardController.java
+++ backend/src/main/java/com/example/backend/controller/board/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
@@ -33,6 +34,7 @@ public class BoardController {
                             .body(Map.of("message", Map.of("type", "error",
                                     "text", STR."\{board.getId()}번 게시물이 수정되지 않았습니다.")));
                 }
+
             } else {
                 return ResponseEntity.badRequest()
                         .body(Map.of("message", Map.of("type", "warning",
@@ -60,7 +62,6 @@ public class BoardController {
                         .body(Map.of("message", Map.of("type", "error"
                                 , "text", "게시글 삭제 중 문제가 발생하였습니다.")));
             }
-
         } else {
             return ResponseEntity.status(403)
                     .body(Map.of("message", Map.of("type", "error"
@@ -78,17 +79,21 @@ public class BoardController {
             @RequestParam(value = "page", defaultValue = "1") Integer page,
             @RequestParam(value = "st", defaultValue = "all") String searchType,
             @RequestParam(value = "sk", defaultValue = "") String keyword) {
+
         System.out.println(searchType);
         System.out.println(keyword);
+
         return service.list(page, searchType, keyword);
     }
 
     @PostMapping("add")
     @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<Map<String, Object>> add(@RequestBody Board board,
-                                                   Authentication authentication) {
+    public ResponseEntity<Map<String, Object>> add(
+            Board board,
+            @RequestParam(value = "files[]", required = false) MultipartFile[] files,
+            Authentication authentication) {
         if (service.validate(board)) {
-            if (service.add(board, authentication)) {
+            if (service.add(board, files, authentication)) {
                 return ResponseEntity.ok()
                         .body(Map.of("message", Map.of("type", "success",
                                         "text", STR."\{board.getId()}번 게시물이 등록되었습니다"),
