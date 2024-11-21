02aa15ce9c08c45fc23d039d4b81e4f84102a7c0
keaimk <fly1043@naver.com>
2024-11-21 15:32:12 +0900

    테이블 생성 쿼리 수정

--- sql/member.sql
+++ sql/member.sql
@@ -2,10 +2,12 @@ USE prj20241114;
 CREATE TABLE member
 (
     id          VARCHAR(20) PRIMARY KEY,
+    email       VARCHAR(300) UNIQUE,
     password    VARCHAR(30) NOT NULL,
     description VARCHAR(1000),
     inserted    DATETIME DEFAULT NOW()
 );
+
 SELECT *
 FROM member;
 
