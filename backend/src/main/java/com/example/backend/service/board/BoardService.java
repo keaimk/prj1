7f00247e7e346e9ae1bce8eb84407bfb1ccfba81
keaimk <fly1043@naver.com>
2024-11-22 12:28:50 +0900

    좋아요

--- backend/src/main/java/com/example/backend/service/board/BoardService.java
+++ backend/src/main/java/com/example/backend/service/board/BoardService.java
@@ -171,14 +171,13 @@ public class BoardService {
 
     public Map<String, Object> getLike(int id, Authentication auth) {
         boolean like = false;
-        if (auth == null) {
+        if (auth != null) {
             Map<String, Object> row = mapper.selectLikeByBoardIdAndMemberId(id, auth.getName());
             if (row != null) {
                 like = true;
             }
         }
         int countLike = mapper.countLike(id);
-
         Map<String, Object> result = Map.of("like", like, "count", countLike);
         return result;
     }
