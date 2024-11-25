6c553a036514b47f4b9a9bd71c2fc97294de8df1
keaimk <fly1043@naver.com>
2024-11-25 11:38:51 +0900

    build.sh 오타 수정

--- backend/build.sh
+++ backend/build.sh
@@ -8,5 +8,5 @@ mv dist ../backend/src/main/resources/static
 cd ../backend
 ./gradlew bootJar
 
-scp -i src/main/resources/secret/key1121.pem build/libs/backend-0.0.1-SNAPSHOT.jar ubuntu@3.35.214.27:./paj.jar
+scp -i src/main/resources/secret/key1121.pem build/libs/backend-0.0.1-SNAPSHOT.jar ubuntu@3.35.214.27:./prj.jar
 ssh -i src/main/resources/secret/key1121.pem ubuntu@3.35.214.27 './run.sh'
